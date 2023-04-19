0fa30691bcfba92c4afc8eb1c3ffd65b528a6792
Danish-chouhan <danishchouhan41@gmail.com>
2023-04-19 10:34:10 +0530

    Edabit

--- algorithum/very-Easy.js
+++ algorithum/very-Easy.js
@@ -4,7 +4,18 @@ function calcAge(num){
 }
 
 
-console.log(calcAge(65)) // 23725
-console.log(calcAge(0)) // 0
-console.log(calcAge(20)) // 7300
+// console.log(calcAge(65)) // 23725
+// console.log(calcAge(0)) // 0
+// console.log(calcAge(20)) // 7300
 
+// -----------------------------------------------------------
+
+
+function nextEdge(n1,n2){
+    return (n1 + n2) -1
+}
+
+
+console.log(nextEdge(8, 10)) // 17
+console.log(nextEdge(5, 7)) // 11
+console.log(nextEdge(9, 2)) // 10
